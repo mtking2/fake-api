9d1f5a23cde5eb77c3250ee8275db6dfa69995ea
Michael King <mtking1123@gmail.com>
2018-08-22 16:57:22 -0400

    update script

--- start.sh
+++ start.sh
@@ -1,19 +1,18 @@
-pkill -f ngrok fake_api
+pkill -f ngrok "puma.*fake_api"
 
 [ -f api.log ] || touch api.log
 
 ruby api.rb >> api.log 2>&1 &
 ngrok http 4567 > /dev/null &
-
 disown
-sleep 1.5
 
-var_location="$HOME/.bash_profile"
-
-old_url="$(grep -oE "(https:\/\/\w+\.ngrok\.io)" $var_location)"
-new_url="$(curl -s localhost:4040/api/tunnels | grep -oE "(https:\/\/\w+\.ngrok\.io)")"
-echo "$new_url --> $(ps -a | grep puma | grep fake_api | grep -oE "localhost:\d+")"
-
-old_url="$(echo $old_url | sed 's/\//\\\//g')"
-new_url="$(echo $new_url | sed 's/\//\\\//g')"
-sed -i '' 's/'"$old_url"'/'"$new_url"'/g' $var_location
+# sleep 1.5
+# var_location="$HOME/.bash_profile"
+#
+# old_url="$(grep -oE "(https:\/\/\w+\.ngrok\.io)" $var_location)"
+# new_url="$(curl -s localhost:4040/api/tunnels | grep -oE "(https:\/\/\w+\.ngrok\.io)")"
+# echo "$new_url --> $(ps -a | grep puma | grep fake_api | grep -oE "localhost:\d+")"
+#
+# old_url="$(echo $old_url | sed 's/\//\\\//g')"
+# new_url="$(echo $new_url | sed 's/\//\\\//g')"
+# sed -i '' 's/'"$old_url"'/'"$new_url"'/g' $var_location
