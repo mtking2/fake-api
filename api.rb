5e647d1d3a70c589c20210e8d2d5c70493012022
Michael King <mtking1123@gmail.com>
2019-08-15 08:51:34 -0400

    add support for custom routes file

--- api.rb
+++ api.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 require 'nokogiri'
 require 'json'
+require_relative 'custom_routes' if File.exist?('custom_routes.rb')
 
-configure {
-  set :server, :puma
-}
+configure { set :server, :puma }
 
 # set this to true if you want to match any and all routes.
 # defaults to JSON and XML specific endpoints while rejecting root and undefined routes.
@@ -12,33 +11,33 @@ configure {
 
 unless @match_all # use JSON and XML specific endpoints. Reject / and undefined
 
-  before ['/json','/json/*'] do
+  before ['/json', '/json/*'] do
     ct = request.media_type
     path = request.path
     method = request.request_method
 
     if ct != 'application/json' && method == 'POST'
-      halt 400, {'Content-Type' => 'text/plain'}, "Invalid Content Type #{ct} for endpoint #{path}"
+      halt 400, { 'Content-Type' => 'text/plain' }, "Invalid Content Type #{ct} for endpoint #{path}"
     end
   end
 
-  before ['/xml','/xml/*'] do
+  before ['/xml', '/xml/*'] do
     ct = request.media_type
     path = request.path
     method = request.request_method
 
     if ct != 'text/xml' && method == 'POST'
-      halt 400, {'Content-Type' => 'text/plain'}, "Invalid Content Type #{ct} for endpoint #{path}"
+      halt 400, { 'Content-Type' => 'text/plain' }, "Invalid Content Type #{ct} for endpoint #{path}"
     end
   end
 
   # GET requests
-  get ['/json','/json/*'] do
+  get ['/json', '/json/*'] do
     content_type :json
     { message: "hello, world!" }.to_json
   end
 
-  get ['/xml','/xml/*'] do
+  get ['/xml', '/xml/*'] do
     content_type 'text/xml'
     Nokogiri::XML::Builder.new { |xml|
       xml.root {
@@ -50,26 +49,26 @@ unless @match_all # use JSON and XML specific endpoints. Reject / and undefined
   end
 
   # POST requests
-  post ['/json','/json/*'] do
+  post ['/json', '/json/*'] do
     request.body.rewind
     json = JSON.parse request.body.read
     puts "I got some JSON: #{json.inspect}"
   end
 
-  post ['/xml','/xml/*'] do
+  post ['/xml', '/xml/*'] do
     request.body.rewind
     xml = request.body.read
-    puts "I got some XML:"
+    puts 'I got some XML:'
     puts Nokogiri::XML(xml)
   end
 
   # respond with 404 for root or undefined route for GET and/or POST
-  get ['/','*a'] do
-    halt 404, {'Content-Type' => 'text/plain'}, "Routing Error: No endpoint matching #{request.path}"
+  get ['/', '*a'] do
+    halt 404, { 'Content-Type' => 'text/plain' }, "Routing Error: No endpoint matching #{request.path}"
   end
 
-  post ['/','*a'] do
-    halt 404, {'Content-Type' => 'text/plain'}, "Routing Error: No endpoint matching #{request.path}"
+  post ['/', '*a'] do
+    halt 404, { 'Content-Type' => 'text/plain' }, "Routing Error: No endpoint matching #{request.path}"
   end
 
 else # match any and all routes for GET & POST and treat as JSON
@@ -80,22 +79,25 @@ else # match any and all routes for GET & POST and treat as JSON
     method = request.request_method
 
     if ct != 'application/json' && method == 'POST'
-      halt 400, {'Content-Type' => 'text/plain'}, "Invalid Content Type #{ct} for endpoint #{path}"
+      halt 400, { 'Content-Type' => 'text/plain' }, "Invalid Content Type #{ct} for endpoint #{path}"
     end
   end
 
   get '/*' do
     content_type :json
-    { message: "hello, world!" }.to_json
+    { message: 'hello, world!' }.to_json
   end
 
   post '/*' do
+    content_type :json
     request.body.rewind
     json = JSON.parse request.body.read
     puts "I got some JSON: #{json.inspect}"
+    json.to_json
   end
 
   patch '/*' do
+    content_type :json
     request.body.rewind
     json = JSON.parse request.body.read
     puts "I got some JSON: #{json.inspect}"
